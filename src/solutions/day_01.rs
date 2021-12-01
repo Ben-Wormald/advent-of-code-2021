bd0669c2cc35c22a2474b4e70f31841e3eaf9184
Ben Wormald <benwormald95@gmail.com>
2021-12-01 21:15:39 +0000

    solve day 01

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -1,8 +1,37 @@
-pub fn solve(input: &str) -> u32 {
-    let lines: Vec<&str> = input.split('\n').collect();
+pub fn solve_part_one(input: &str) -> u32 {
+    let depths: Vec<usize> = input
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
 
-    for line in &lines {
-        
-    }
-    0
+    let mut increase_count = 0;
+
+    depths
+        .into_iter()
+        .reduce(|prev_depth, depth| {
+            if depth > prev_depth { increase_count += 1 };
+            depth
+        });
+
+    increase_count
+}
+
+pub fn solve(input: &str) -> u32 {
+    let depths: Vec<usize> = input
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    let mut increase_count = 0;
+
+    depths
+        .windows(3)
+        .reduce(|prev_depth, depth| {
+            if depth.into_iter().sum::<usize>() > prev_depth.into_iter().sum() {
+                increase_count += 1;
+            }
+            depth
+        });
+
+    increase_count
 }
