eac67c81712d45e09cda60a4f40fd5415650c07c
Ben-Wormald <benwormald95@gmail.com>
2021-12-03 17:32:50 +0000

    solve day 03

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -7,6 +7,14 @@ fn binary_to_decimal(binary: Vec<usize>) -> usize {
     })
 }
 
+fn binary_str_to_decimal(binary: &str) -> usize {
+    binary_to_decimal(
+        binary.chars()
+            .map(|bit| bit.to_string().parse().unwrap())
+            .collect()
+    )
+}
+
 pub fn solve_part_one(input: &str) -> usize {
     let bit_count = *&input.lines().next().unwrap().len();
     let mut one_counts = vec![0; bit_count];
@@ -34,7 +42,20 @@ pub fn solve_part_one(input: &str) -> usize {
     gamma_rate * epsilon_rate
 }
 
-fn filter_lines<'a>(lines: &Vec<&'a str>, criterion: char, pos: usize) -> Vec<&'a str> {
+fn filter_lines<'a>(lines: &Vec<&'a str>, pos: usize, use_most_common: bool) -> Vec<&'a str> {
+    let (mut zero_count, mut one_count) = (0, 0);
+
+    lines.iter()
+        .for_each(|line|
+            if line.chars().nth(pos).unwrap() == '0' { zero_count += 1 } else { one_count += 1 }
+        );
+
+    let criterion = if use_most_common {
+        if zero_count > one_count { '0' } else { '1' }
+    } else {
+        if zero_count > one_count { '1' } else { '0' }
+    };
+
     lines.iter()
         .filter(|line|
             line.chars().nth(pos).unwrap() == criterion
@@ -43,54 +64,20 @@ fn filter_lines<'a>(lines: &Vec<&'a str>, criterion: char, pos: usize) -> Vec<&'
         .collect()
 }
 
-fn get_match<'a>(lines: &Vec<&'a str>, criteria: Vec<char>, pos: usize) -> &'a str {
-    let filtered_lines = filter_lines(lines, criteria[pos], pos);
+fn get_match<'a>(lines: &Vec<&'a str>, pos: usize, use_most_common: bool) -> &'a str {
+    let filtered_lines = filter_lines(lines, pos, use_most_common);
     match filtered_lines.len() {
-        len if len > 1 => get_match(&filtered_lines, criteria, pos + 1),
+        len if len > 1 => get_match(&filtered_lines, pos + 1, use_most_common),
         1 => filtered_lines[0],
         _ => panic!("no matches!")
     }
 }
 
-fn binary_str_to_decimal(binary: &str) -> usize {
-    binary_to_decimal(
-        binary.chars()
-            .map(|bit| bit.to_string().parse().unwrap())
-            .collect()
-    )
-}
-
 pub fn solve(input: &str) -> usize {
-    let bit_count = *&input.lines().next().unwrap().len();
-    let mut one_counts = vec![0; bit_count];
-    let mut total_lines = 0;
-
     let lines: Vec<&str> = input.lines().collect();
-    
-    lines.iter()
-        .for_each(|line| {
-            line.chars()
-                .enumerate()
-                .for_each(|(pos, digit)|
-                    if digit == '1' { one_counts[pos] += 1 }
-                );
-            total_lines += 1;
-        });
 
-    let threshold = total_lines / 2;
-
-    let oxygen_criteria: Vec<char> = one_counts.iter()
-        .map(|count|
-            if count >= &threshold { '1' } else { '0' }
-        )
-        .collect();
-
-    let co2_criteria: Vec<char> = oxygen_criteria.iter()
-        .map(|bit| if *bit == '0' { '1' } else { '0' })
-        .collect();
-
-    let oxygen_rating = get_match(&lines, oxygen_criteria, 0);
-    let co2_rating = get_match(&lines, co2_criteria, 0);
+    let oxygen_rating = get_match(&lines, 0, true);
+    let co2_rating = get_match(&lines, 0, false);
 
     let oxygen_rating = binary_str_to_decimal(oxygen_rating);
     let co2_rating = binary_str_to_decimal(co2_rating);
