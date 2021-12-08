31b8951815f27a87753894415711053bf4602d7a
Ben-Wormald <benwormald95@gmail.com>
2021-12-08 12:09:32 +0000

    tidy up day 08 solution

--- src/solutions/day_08.rs
+++ src/solutions/day_08.rs
@@ -18,64 +18,56 @@ pub fn solve_part_one(input: &str) -> usize {
 
 #[derive(Debug)]
 struct Digit<'a> {
-    example: Option<&'a &'a str>,
+    example: &'a str,
     value: usize,
 }
 
-fn contains_all(digit_a: &str, digit_b: Option<&&str>) -> bool {
-    if let Some(b) = digit_b {
-        b.chars().all(|part| digit_a.contains(part))
-    } else {
-        false
-    }
+fn contains_all(digit_a: &str, digit_b: &str) -> bool {
+    digit_b.chars().all(|part| digit_a.contains(part))
 }
 
-fn contains_n(digit_a: &str, digit_b: Option<&&str>, n: usize) -> bool {
-    if let Some(b) = digit_b {
-        b.chars().filter(|part| digit_a.contains(*part)).count() == n
-    } else {
-        false
-    }
+fn contains_n(digit_a: &str, digit_b: &str, n: usize) -> bool {
+    digit_b.chars().filter(|part| digit_a.contains(*part)).count() == n
 }
 
-fn is_equal(digit_a: &str, digit_b: Option<&&str>) -> bool {
-    if let Some(b) = digit_b {
-        contains_all(digit_a, digit_b) && contains_all(b, Some(&digit_a))
-    } else {
-        false
-    }
+fn is_equal(digit_a: &str, digit_b: &str) -> bool {
+    contains_all(digit_a, digit_b) && contains_all(digit_b, digit_a)
 }
 
 fn get_value(line: &str) -> usize {
-    let parts: Vec<&str> = line.split(" | ").collect();
-    let digits: Vec<&str> = parts[0].split(" ").collect();
-    let output_digits: Vec<&str> = parts[1].split(" ").collect();
+    let parts: Vec<Vec<&str>> = line
+        .split(" | ")
+        .map(|part| part.split(" ").collect())
+        .collect();
+
+    let digits = &parts[0];
+    let output_digits = &parts[1];
     let mut mappings: Vec<Digit> = vec!();
 
-    let one = digits.iter().find(|digit| digit.len() == 2);
-    let seven = digits.iter().find(|digit| digit.len() == 3);
-    let four = digits.iter().find(|digit| digit.len() == 4);
-    let eight = digits.iter().find(|digit| digit.len() == 7);
-
-    let six = digits.iter().find(|digit|
-        digit.len() == 6 && !contains_all(digit, one)
-    );
-    let zero = digits.iter().find(|digit|
-        digit.len() == 6 && !contains_all(digit, four) && !is_equal(digit, six)
-    );
-    let nine = digits.iter().find(|digit|
-        digit.len() == 6 && !is_equal(digit, six) && !is_equal(digit, zero)
-    );
+    let one = digits.iter().find(|digit| digit.len() == 2).unwrap();
+    let seven = digits.iter().find(|digit| digit.len() == 3).unwrap();
+    let four = digits.iter().find(|digit| digit.len() == 4).unwrap();
+    let eight = digits.iter().find(|digit| digit.len() == 7).unwrap();
 
     let three = digits.iter().find(|digit|
         digit.len() == 5 && contains_all(digit, one)
-    );
+    ).unwrap();
     let two = digits.iter().find(|digit|
         digit.len() == 5 && contains_n(digit, four, 2) && !is_equal(digit, three)
-    );
+    ).unwrap();
     let five = digits.iter().find(|digit|
         digit.len() == 5 && !is_equal(digit, two) && !is_equal(digit, three)
-    );
+    ).unwrap();
+
+    let six = digits.iter().find(|digit|
+        digit.len() == 6 && !contains_all(digit, one)
+    ).unwrap();
+    let zero = digits.iter().find(|digit|
+        digit.len() == 6 && !contains_all(digit, four) && !is_equal(digit, six)
+    ).unwrap();
+    let nine = digits.iter().find(|digit|
+        digit.len() == 6 && !is_equal(digit, six) && !is_equal(digit, zero)
+    ).unwrap();
 
     mappings.push(Digit { example: zero, value: 0 });
     mappings.push(Digit { example: one, value: 1 });
@@ -94,8 +86,8 @@ fn get_value(line: &str) -> usize {
                 "{}{}",
                 digits,
                 mappings.iter()
-                    .find(|example_digit|
-                        is_equal(digit, example_digit.example)
+                    .find(|mapping|
+                        is_equal(digit, mapping.example)
                     ).unwrap()
                     .value,
             )
